0fa4b713015b430ccb24eca056704d91859d6f76
Vakhrushev <konstantinvakhrushev@Noutbuknstantin.mshome.net>
2024-07-19 16:20:09 +0500

    fix linter revive

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
+	"net"
+	"time"
+
 	"github.com/vakhrushevk/auth-service/pkg/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
-	"log"
-	"net"
-	"time"
 )
 
 const grpcPort = 50051
@@ -20,6 +21,7 @@ type server struct {
 	user_v1.UnimplementedUserV1Server
 }
 
+//nolint:revive
 func (s server) Create(ctx context.Context, request *user_v1.CreateRequest) (*user_v1.CreateResponse, error) {
 	log.Println("Received Create request")
 	log.Printf("Name: %s\n Email: %s\n Password: %s\n PasswordConfirm %s\n Role %s",
@@ -28,6 +30,7 @@ func (s server) Create(ctx context.Context, request *user_v1.CreateRequest) (*us
 	return &user_v1.CreateResponse{Id: 1}, nil
 }
 
+//nolint:revive
 func (s server) Get(ctx context.Context, request *user_v1.GetRequest) (*user_v1.GetResponse, error) {
 	if request.Id == 0 {
 		return &user_v1.GetResponse{}, errors.New("failed to request: empty id")
@@ -45,6 +48,7 @@ func (s server) Get(ctx context.Context, request *user_v1.GetRequest) (*user_v1.
 	}, nil
 }
 
+//nolint:revive
 func (s server) Update(ctx context.Context, request *user_v1.UpdateRequest) (*emptypb.Empty, error) {
 	log.Println("Received Update request")
 	log.Println("id: ", request.GetId())
@@ -54,6 +58,7 @@ func (s server) Update(ctx context.Context, request *user_v1.UpdateRequest) (*em
 	return nil, nil
 }
 
+//nolint:revive
 func (s server) Delete(ctx context.Context, request *user_v1.DeleteRequest) (*emptypb.Empty, error) {
 	log.Println("Received Delete request")
 	log.Println("id: ", request.GetId())
